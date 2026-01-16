588899dc135b88df9cf27603891b19a40c0654dc
Aldi Tegar Prakoso <alditegarprakoso@gmail.com>
2026-01-17 05:22:21 +0700

    add function example

---
+++ kenapa-koding/typescript-module-1/src/function.ts
@@ -0,0 +1,7 @@
+// Function di TypeScript
+
+// Function di bawah ini menggunakan tipe data string sebagai parameter dan mengembalikan string.
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+}
+console.log(greet("Alice"));
